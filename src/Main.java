bd692e8d7cb53245379b655a97352c941fd4cc0b
Hestia <ducanhvu@nhatanh.edu.vn>
2021-03-11 22:22:53 -0500

    Add count blob + recursion

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,12 @@ public class Main {
 
     /**
      * Method Name: readGridData
-     * Author: Duc Anh Vu
+     * @author Duc Anh Vu
      * Creation Date: March 11, 2021
      * Modified Date: March 11, 2021
      * Description: Read the grid data from text file and put it in to an 2D array
-     * Parameters: Scanner to check if the file has next line or end
-     * Return Value: N/A
+     * @param scanner Scanner to check if the file has next line or end
+     * @return array grid
      * Data Type: Void
      * Dependencies: N/A
      * Throws/Exceptions: N/A
@@ -39,11 +39,11 @@ public class Main {
 
     /**
      * Method Name: printGrid
-     * Author: Duc Anh Vu
+     * @author Duc Anh Vu
      * Creation Date: March 11, 2021
      * Modified Date: March 11, 2021
      * Description: Print out the grid from the array in parameter
-     * Parameters: Grid array with character type
+     * @param grid array grid (character type)
      * Return Value: N/A
      * Data Type: Void
      * Dependencies: N/A
@@ -58,9 +58,50 @@ public class Main {
             }
             System.out.println();
         }
-        System.out.println();
     } // End of printGrid
 
+    public static int findShapes(char[][] grid, int rowCount, int columnCount) {
+        int[][] visited = new int[rowCount][columnCount]; // all false from beginning
+
+        int count = 0;
+
+        for (int i = 0; i < rowCount; i++)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                if (grid[i][j] == 'X' && visited[i][j] == 0 ) // Not arrived dot
+                {
+                    wentThrough(i,j, grid, visited, rowCount, columnCount);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    public static int wentThrough(int i, int j, char[][] grid, int[][] arrived, int rowCount, int columnCount)
+    {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (i >= rowCount || j >= columnCount)
+            return 0;
+
+        if (arrived[i][j] == 1) // already arrived
+            return 1;
+
+        if (grid[i][j] == '.') // Not a dot
+            return 0;
+
+        arrived[i][j] = 1;
+
+    // recursively mark all the 4 adjacent cells - right, left, up and down
+        return wentThrough(i+1, j, grid, arrived, rowCount, columnCount)
+                + wentThrough(i-1, j, grid, arrived, rowCount, columnCount)
+                + wentThrough(i, j+1, grid, arrived, rowCount, columnCount)
+                + wentThrough(i, j-1, grid, arrived, rowCount, columnCount);
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -72,9 +113,10 @@ public class Main {
             while (scanner.hasNextLine()) {
                 char[][] grid = readGridData(scanner);
                 printGrid(grid);
+                System.out.println("This rectangle has " + findShapes(grid, grid.length, grid[0].length) + " shapes");
+                System.out.println();
             }
         }
         inputKeyboard.close();
     } // End main
-
 }
